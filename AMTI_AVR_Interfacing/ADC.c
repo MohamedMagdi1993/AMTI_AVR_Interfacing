5fed01688165407b2183e8d10901f8a555eeb8c8
MohamedMagdi1993 <mohamed.magdi1993@outlook.com>
2021-10-30 08:19:35 +0200

    implement of ADC driver

--- AMTI_AVR_Interfacing/ADC.c
+++ AMTI_AVR_Interfacing/ADC.c
@@ -4,12 +4,33 @@
  * Created: 10/28/2021 3:25:17 PM
  *  Author: Mohamed Magdi
  */ 
-
-ReturnValueType Adc_Init(void) 
+#include "ADC.h"
+void Adc_Init(void) 
 {
-	return OK ;
+// 	DDRA &=~ (1<<PA1) ; // 1
+// 	ADCSRA |= (1<<ADEN) ; // 2
+// 	ADCSRA |= (1<<ADPS0) | (1<<ADPS1) | (1<<ADPS2);
+// 	SetBit(ADMUX,REFS0) ;
+// 	ClearBit(ADMUX,REFS1);
+// 	SetBit(ADMUX,MUX0);
+// 	ADMUX &= ~ (1<<MUX1) & ~ (1<<MUX2) & ~ (1<<MUX3) & ~ (1<<MUX4);
+	ADCSRA |= (1<<ADEN) ;
+ 	SetBit(ADMUX,REFS0) ;
+ 	ClearBit(ADMUX,REFS1);
+	ADCSRA |= (1<<ADPS0) | (1<<ADPS1) | (1<<ADPS2);
 }
-ReturnValueType Adc_GetValue(uint8 * Adc_Value) 
+
+void Adc_ChannelInit(uint8 ADC_Channel)
 {
-	return OK;
+	
+			Dio_PinSetDirection(A,ADC_Channel,PinInput);
+			ADMUX= (ADMUX & ~0x1F) | (ADC_Channel & 0x1F) ;
+	
+}
+void Adc_ReadChannel(uint8 ADC_Channel , uint16* Adc_Data)
+{
+	ADCSRA |= (1<<ADSC) ;
+	while (GetBit(ADCSRA,ADIF) == 0);
+	*Adc_Data = ADCL ;
+	*Adc_Data |= (uint16) (ADCH<<8) ;
 }
